1e8715ac0827ae15e5c9ae602ef7e21787df0bcd
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-09 13:11:19 +0900

    :recycle: NW追加

---
+++ envs/vcn.tf
@@ -0,0 +1,245 @@
+/************************************************************
+VCN
+************************************************************/
+resource "oci_core_vcn" "vcn" {
+  compartment_id = oci_identity_compartment.workload.id
+  cidr_block     = "10.0.0.0/16"
+  display_name   = "vcn"
+  # 最大15文字の英数字
+  # 文字から始めること
+  # ハイフンとアンダースコアは使用不可
+  # 後から変更不可
+  dns_label      = "vcn"
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+/************************************************************
+Security List
+************************************************************/
+resource "oci_core_security_list" "sl" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "nothing-security-list"
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+/************************************************************
+Subnet
+************************************************************/
+### For Bastion
+resource "oci_core_subnet" "private_bastion" {
+  compartment_id             = oci_identity_compartment.workload.id
+  vcn_id                     = oci_core_vcn.vcn.id
+  cidr_block                 = "10.0.1.0/24"
+  display_name               = "private-bastion"
+  # 最大15文字の英数字
+  # 文字から始めること
+  # ハイフンとアンダースコアは使用不可
+  # 後から変更不可
+  dns_label                  = "bastionnw"
+  security_list_ids          = [oci_core_security_list.sl.id]
+  # prohibit_internet_ingress と prohibit_public_ip_on_vnic は 同様の動き
+  # そのため、２つのパラメータの true/false を互い違いにするとconflictでエラーとなる
+  # 基本的には、値を揃えるか、どちらか一方を明記すること
+  prohibit_internet_ingress  = true
+  prohibit_public_ip_on_vnic = true
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+### For Oracle Linux
+resource "oci_core_subnet" "private_oracle" {
+  compartment_id             = oci_identity_compartment.workload.id
+  vcn_id                     = oci_core_vcn.vcn.id
+  cidr_block                 = "10.0.2.0/24"
+  display_name               = "private-oracle"
+  # 最大15文字の英数字
+  # 文字から始めること
+  # ハイフンとアンダースコアは使用不可
+  # 後から変更不可
+  dns_label                  = "oraclenw"
+  security_list_ids          = [oci_core_security_list.sl.id]
+  # prohibit_internet_ingress と prohibit_public_ip_on_vnic は 同様の動き
+  # そのため、２つのパラメータの true/false を互い違いにするとconflictでエラーとなる
+  # 基本的には、値を揃えるか、どちらか一方を明記すること
+  prohibit_internet_ingress  = true
+  prohibit_public_ip_on_vnic = true
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+### For Windows Server
+resource "oci_core_subnet" "private_windows" {
+  compartment_id             = oci_identity_compartment.workload.id
+  vcn_id                     = oci_core_vcn.vcn.id
+  cidr_block                 = "10.0.3.0/24"
+  display_name               = "private-windows"
+  # 最大15文字の英数字
+  # 文字から始めること
+  # ハイフンとアンダースコアは使用不可
+  # 後から変更不可
+  dns_label                  = "windowsnw"
+  security_list_ids          = [oci_core_security_list.sl.id]
+  # prohibit_internet_ingress と prohibit_public_ip_on_vnic は 同様の動き
+  # そのため、２つのパラメータの true/false を互い違いにするとconflictでエラーとなる
+  # 基本的には、値を揃えるか、どちらか一方を明記すること
+  prohibit_internet_ingress  = true
+  prohibit_public_ip_on_vnic = true
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+# /************************************************************
+# Internet Gateway
+# ************************************************************/
+# resource "oci_core_internet_gateway" "igw" {
+#   compartment_id = oci_identity_compartment.workload.id
+#   vcn_id         = oci_core_vcn.vcn.id
+#   display_name   = "igw"
+#   defined_tags = {
+#     format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+#     format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+#   }
+# }
+
+# /************************************************************
+# Route Table
+# ************************************************************/
+### For Bastion
+resource "oci_core_route_table" "rtb_bastion" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "rtb-bastion"
+#   route_rules {
+#     network_entity_id = oci_core_internet_gateway.igw.id
+#     destination       = "0.0.0.0/0"
+#     destination_type  = "CIDR_BLOCK"
+#   }
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+resource "oci_core_route_table_attachment" "attachment_bastion" {
+  subnet_id      = oci_core_subnet.private_bastion.id
+  route_table_id = oci_core_route_table.rtb_bastion.id
+}
+
+### For Oracle Linux
+resource "oci_core_route_table" "rtb_oracle" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "rtb-oracle"
+#   route_rules {
+#     network_entity_id = oci_core_internet_gateway.igw.id
+#     destination       = "0.0.0.0/0"
+#     destination_type  = "CIDR_BLOCK"
+#   }
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+resource "oci_core_route_table_attachment" "attachment_oracle" {
+  subnet_id      = oci_core_subnet.private_oracle.id
+  route_table_id = oci_core_route_table.rtb_oracle.id
+}
+
+### For Windows Server
+resource "oci_core_route_table" "rtb_windows" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "rtb-windows"
+#   route_rules {
+#     network_entity_id = oci_core_internet_gateway.igw.id
+#     destination       = "0.0.0.0/0"
+#     destination_type  = "CIDR_BLOCK"
+#   }
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+resource "oci_core_route_table_attachment" "attachment_windows" {
+  subnet_id      = oci_core_subnet.private_windows.id
+  route_table_id = oci_core_route_table.rtb_windows.id
+}
+
+# /************************************************************
+# Network Security Group
+# ************************************************************/
+### For Oracle Linux
+resource "oci_core_network_security_group" "sg_oracle" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "sg-oracle"
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+# resource "oci_core_network_security_group_security_rule" "sg_rule_1" {
+#   network_security_group_id = oci_core_network_security_group.sg.id
+#   protocol                  = "6"
+#   direction                 = "INGRESS"
+#   source                    = var.source_ip
+#   stateless                 = false
+#   source_type               = "CIDR_BLOCK"
+#   tcp_options {
+#     destination_port_range {
+#       min = 22
+#       max = 22
+#     }
+#   }
+# }
+
+# resource "oci_core_network_security_group_security_rule" "sg_rule_2" {
+#   network_security_group_id = oci_core_network_security_group.sg.id
+#   protocol                  = "6"
+#   direction                 = "INGRESS"
+#   source                    = var.source_ip
+#   stateless                 = false
+#   source_type               = "CIDR_BLOCK"
+#   tcp_options {
+#     destination_port_range {
+#       min = 3389
+#       max = 3389
+#     }
+#   }
+# }
+
+# resource "oci_core_network_security_group_security_rule" "sg_rule_3" {
+#   network_security_group_id = oci_core_network_security_group.sg.id
+#   protocol                  = "all"
+#   direction                 = "EGRESS"
+#   destination               = "0.0.0.0/0"
+#   stateless                 = false
+#   destination_type          = "CIDR_BLOCK"
+# }
+
+### For Windows Server
+resource "oci_core_network_security_group" "sg_windows" {
+  compartment_id = oci_identity_compartment.workload.id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "sg-windows"
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
