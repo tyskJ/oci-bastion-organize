b89b48aa0f60ffe30d7bbfda57b8f9e136443153
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-10 18:56:53 +0900

    :recycle: For Each / Dynamic でリファクタリング

--- envs/logs.tf
+++ envs/logs.tf
@@ -1,48 +1,48 @@
-/************************************************************
-Log Group
-************************************************************/
+# /************************************************************
+# Log Group
+# ************************************************************/
 ##### For VCN Flow Logs
-resource "oci_logging_log_group" "lg_vcn_flow_logs" {
+resource "oci_logging_log_group" "log_group" {
+  for_each = local.flowlog_sets
+
   compartment_id = oci_identity_compartment.workload.id
-  display_name   = "lg-vcn-flow-logs"
-  description    = "For VCN Flow Logs"
-  defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-  }
+  display_name   = each.value.log_group_name
+  description    = each.value.log_group_desc
+  defined_tags   = local.common_defined_tags
 }
 
-/************************************************************
-Capture Filter - Flow log
-************************************************************/
-resource "oci_core_capture_filter" "cf_vcn_flow_logs" {
+# /************************************************************
+# Capture Filter - Flow log
+# ************************************************************/
+resource "oci_core_capture_filter" "capture_filter" {
+  for_each = local.flowlog_sets
+
   compartment_id = oci_identity_compartment.workload.id
-  display_name   = "cf-vcn-flow-logs"
+  display_name   = each.value.capture_name
   filter_type    = "FLOWLOG"
-  # TCP All Rule
-  flow_log_capture_filter_rules {
-    priority         = 0
-    sampling_rate    = 1
-    is_enabled       = true
-    flow_log_type    = "ALL"
-    rule_action      = "INCLUDE"
-    source_cidr      = "0.0.0.0/0"
-    destination_cidr = "0.0.0.0/0"
-    protocol         = "6"
-    tcp_options {}
+  dynamic "flow_log_capture_filter_rules" {
+    for_each = each.value.capture_rules
+    content {
+      priority         = flow_log_capture_filter_rules.value.priority
+      sampling_rate    = flow_log_capture_filter_rules.value.sampling_rate
+      is_enabled       = flow_log_capture_filter_rules.value.is_enabled
+      flow_log_type    = flow_log_capture_filter_rules.value.flow_log_type
+      rule_action      = flow_log_capture_filter_rules.value.rule_action
+      source_cidr      = flow_log_capture_filter_rules.value.source_cidr
+      destination_cidr = flow_log_capture_filter_rules.value.destination_cidr
+      protocol         = flow_log_capture_filter_rules.value.protocol
+    }
   }
-  defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-  }
+  defined_tags = local.common_defined_tags
 }
 
-/************************************************************
-Logs
-************************************************************/
-##### VCN Flow Logs
-resource "oci_logging_log" "vcn_flow_logs" {
-  display_name = "logs-vcn-flow-logs-points-subnet"
+# /************************************************************
+# Logs - VCN Flow Logs
+# ************************************************************/
+resource "oci_logging_log" "flow" {
+  for_each = local.flowlog_sets
+
+  display_name = each.value.log_name
   is_enabled   = true
   log_type     = "SERVICE"
   configuration {
@@ -52,16 +52,13 @@ resource "oci_logging_log" "vcn_flow_logs" {
       source_type = "OCISERVICE"
       service     = "flowlogs"
       category    = "subnet"
-      resource    = oci_core_subnet.private_bastion.id
+      resource    = each.value.subnet_id
       parameters = {
-        capture_filter = oci_core_capture_filter.cf_vcn_flow_logs.id
+        capture_filter = oci_core_capture_filter.capture_filter[each.key].id
       }
     }
   }
-  log_group_id       = oci_logging_log_group.lg_vcn_flow_logs.id
+  log_group_id       = oci_logging_log_group.log_group[each.key].id
   retention_duration = 30
-  defined_tags = {
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-  }
+  defined_tags       = local.common_defined_tags
 }
